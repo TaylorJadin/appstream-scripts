225f5b9dcff52b59dbf3d261f913d76620cf8113
kindamessi <nick.plank@snc.edu>
2021-02-10 13:40:29 -0600

    update
    update to login and logout session scripts

--- login.ps1
+++ login.ps1
@@ -1,5 +1,17 @@
+#################################################################################################
+## login.ps1
+## Runs as part of the login process of AppStream for some fleets
 ## Script will try to copy a user's files from their Desktop, Downloads, and Documents folders
-## Runs at login in SNCStudent stack
+## Script is meant to copy any files from a user's previous session to their current session.
+## Script is pulled to computer during the login process.
+## Login.json triggers a script (ScriptPull.ps1) that pulls our repo down. 
+## ScriptPull.ps1 runs this scripts during it's Invoke-Expression command, at the end.
+##
+## St. Norbert College
+## 02/10/21
+## Nick Plank and Taylor Jadin
+#################################################################################################
+
 $visDesk="C:\PhotonUser\Desktop"
 $visDocs="C:\PhotonUser\Documents"
 $visDown="C:\PhotonUser\Downloads"
@@ -10,31 +22,32 @@ $deskPath="$homeFolder\Desktop"
 $docsPath="$homeFolder\Documents"
 $downPath="$homeFolder\Downloads"
 
-## Check if D:\PhotonUser\My Files\Desktop exists.  If it doesn't, script will create the directory
-## This is a hidden directory
-
+## Check if D:\PhotonUser\My Files\Desktop exists.  
 
 if ( Test-Path $deskPath ){
-    .\Robocopy.exe "$deskPath" "$visDesk" /e /it
+    Robocopy.exe "$deskPath" "$visDesk" /mir /xf *.lnk
 }
 else {
-    mkdir "$deskPath"
-    $hideDesk=get-item "$deskPath" -Force
-    $hideDesk.attributes="Hidden"
+    Write-Host "Must be first login"
+    #mkdir "$deskPath"
+    #$hideDesk=get-item "$deskPath" -Force
+    #$hideDesk.attributes="Hidden"
 }
 if ( Test-Path $docsPath ){
-    .\Robocopy.exe "$docsPath" "$visDocs" /e /it
+    Robocopy.exe "$docsPath" "$visDocs" /mir /xf *.lnk
 }
 else {
-    mkdir "$docsPath"
-    $hideDocs=get-item "$docsPath" -Force
-    $hideDocs.attributes="Hidden"
+    Write-Host "Must be first login"
+    #mkdir "$docsPath"
+    #$hideDocs=get-item "$docsPath" -Force
+    #$hideDocs.attributes="Hidden"
 }
 if ( Test-Path $downPath ){
-    .\Robocopy.exe "$downPath" "$visDown" /e /it
+    Robocopy.exe "$downPath" "$visDown" /mir /xf *.lnk
 }
 else {
-    mkdir "$downPath"
-    $hideDown=get-item "$downPath" -Force
-    $hideDesk.attributes="Hidden"
+    Write-Host "Must be first login"
+    #mkdir "$downPath"
+    #$hideDown=get-item "$downPath" -Force
+    #$hideDesk.attributes="Hidden"
 }
--- logout.ps1
+++ logout.ps1
@@ -1,3 +1,17 @@
+#################################################################################################
+## logout.ps1
+## Runs when user logs out of AppStream, and machine starts the process of complete annihilation.
+## Script will try to copy a user's files from their Desktop, Downloads, and Documents folders
+## Script is meant to copy any files a user has created, so they don't get deleted accidentally
+## Script is pulled to computer during the login process.
+## Login.json triggers a script (ScriptPull.ps1) that pulls our repo down. 
+## This script runs as system, triggered by config.json in C:\AppStream\SessionScripts\config.json
+##
+## St. Norbert College
+## 02/10/21
+## Nick Plank and Taylor Jadin
+#################################################################################################
+
 ## Script copies files from user's Desktop, Downloads, and Documents folders to their home folder.
 ## We do this so their content doesn't accidentally get deleted.
 ## Runs at login in SNCStudent stack
