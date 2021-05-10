816ea002d2feb3c536734ef1c13f4ed0c086c32c
Taylor Jadin <jadin.taylor@gmail.com>
2021-05-10 11:30:10 -0500

    Update login.ps1

--- login.ps1
+++ login.ps1
@@ -67,7 +67,6 @@ function restoreFiles {
 
 ### Main ###
 waitForFSMounts
-createlogLoc
 restoreFiles >> $log
 
 #cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /sethst:ec2-23-20-94-46.compute-1.amazonaws.com
