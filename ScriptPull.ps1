11dceb2822d01df2f2e722a123d7c16736b7a88d
Taylor Jadin <jadin.taylor@gmail.com>
2021-05-10 10:29:33 -0500

    Update ScriptPull.ps1

--- ScriptPull.ps1
+++ ScriptPull.ps1
@@ -2,6 +2,7 @@ $repo="https://github.com/TaylorJadin/appstream-scripts/archive/main.zip"
 $repoLoc="C:\AppStream\SessionScripts\RepoCopy"
 $zipLoc="$repoLoc\main.zip"
 $exZip="$repoLoc\Scripts"
+$logLoc="D:\PhotonUser\My Files\Home Folder\Logs\login.log"
 
 mkdir "$repoLoc"
 mkdir "$exZip"
@@ -9,4 +10,4 @@ mkdir "$exZip"
 Invoke-WebRequest -Uri $repo -OutFile $zipLoc
 Expand-Archive -LiteralPath $zipLoc -DestinationPath $exZip
 
-Invoke-Expression "$exZip\appstream-scripts-main\login.ps1"
+Invoke-Expression "$exZip\appstream-scripts-main\login.ps1" > $logLoc
