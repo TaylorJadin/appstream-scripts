a7c205746c6912778cc8dae72b1f5bf960796ead
kindamessi <63030059+kindamessi@users.noreply.github.com>
2021-07-06 09:33:30 -0500

    Update login.ps1
    Account for non-settings profile enabled desktops.

--- login.ps1
+++ login.ps1
@@ -14,11 +14,18 @@
 
 
 ### Config ###
-$visDesk="D:\PhotonUser\Desktop"
-$visDocs="D:\PhotonUser\Documents"
-$visDown="D:\PhotonUser\Downloads"
+# Apparently D: is where Amazon puts the home folder if you have settings profile enabled.
+#$visDesk="D:\PhotonUser\Desktop"
+#$visDocs="D:\PhotonUser\Documents"
+#$visDown="D:\PhotonUser\Downloads"
 
-$homeFolder="D:\PhotonUser\My Files\Home Folder"
+#$homeFolder="D:\PhotonUser\My Files\Home Folder"
+
+$visDesk="C:\PhotonUser\Desktop"
+$visDocs="C:\PhotonUser\Documents"
+$visDown="C:\PhotonUser\Downloads"
+
+$homeFolder="C:\PhotonUser\My Files\Home Folder"
 
 $deskPath="$homeFolder\Desktop"
 $docsPath="$homeFolder\Documents"
